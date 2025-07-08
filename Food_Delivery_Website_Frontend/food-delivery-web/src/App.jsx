a2723cb4b1c21c5b9761b3ad6660c07664689f76
bipasha <bipashalamsal@gmail.com>
2025-07-09 00:17:26 +0530

    in app.jsx changes made

--- Food_Delivery_Website_Frontend/food-delivery-web/src/App.jsx
+++ Food_Delivery_Website_Frontend/food-delivery-web/src/App.jsx
@@ -95,47 +95,9 @@ export default function App() {
 
 
 
-//user flow banaune
-//image in user persona and location adding
-//response add garne-
-//trello ko screenshot rakhni---
-//nelsosns- ko ma figma ko ki real preoduct ko screenshot-
-//design principle ma real product ko ??
-//design pricnipal ma 1/2 ota add garxu ajha--
-//development methodology aunu agadi user flow diagram(--decision/ui diagram)
 
 
-// In order history, in customer side, the order should be clickable and it should show the order details.
-// Offers in restaurant side, customer should be able to click it only once else it should be disabled.
-// without login home page, design text of about us should be smaller.
-// If menu is in unavailable state, in customer side that unavailable menu should not be clickable and in text-not available should also be seen.
-// Track Order in customer side, it should be in real time according to the order status.
-//in track order, if no order then it should show no order placed yet.
-//in ind res, default image not showing, it should show.
-// If notification or profile is clicked then it should be highlighted with color.
-// In customer side, offers-image should be shown----
-// Search the menus and restaurant in home page and individual restaurant.
-// For footer, in customer side, it should be navigated--
-// Online/Offline toggle in restaurant side funcionality should be implemented.
-// Forgot Password in customer side and restaurant side
-//FOOTER FIX-
-//if restaurant rejects-order rejected then it should go to order history of restaurant side and in customer side---  if rejected then in notification it should be shown as rejected and in track order- delivered and rejected both should show no order yet placed.
-//btn size same in all, color, used same, --
-//in order history, only delivered order should be shown- not pending one.
-//basket,bag text similarity huna parxaaa
-//checkout page after confirm order- basket should be removed and text like special instuction should be rmeoved,
-//location in home page after login-- should get
-//various Ongoing orders for that customer- its details should be shown in track order section.
-//button color change- of order history inside- one
-//product realistic huna paryo, payment gateway halne
-//in home page ma----image ma focus garne high quality wala jun chai scroll huna milne banaune
-//offline online ko satto ma close open rakhne in customer page
-//add note add garni--for each product,
-//picture ma click garda pani add garna parxa
 
-//image scrollable-, search btn chai location ko side ma..
-//plus - huna paryo--
-//color change garne--
-//checkout garepaxi matra login wala garna paryoo-
-//notification ma numbering halne--
-//aja bhyauxu sab yeskoooo realistic product xa ki nai herxu.
+
+
+
